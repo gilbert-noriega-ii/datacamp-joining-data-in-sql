575b649bbc408d6d6063ad839115e086f705883e
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-01-30 12:57:19 -0600

    initial commit to github

---
+++ set_theory_clauses.sql
@@ -0,0 +1,73 @@
+/* populations table fields are 
+
+pop_id
+country_code
+year 
+fertility_rate
+life_expectancy
+size
+
+*/
+
+/* countries table fields are 
+
+code
+name 
+continent
+region
+surface_area
+indep_year
+local_name
+gov_form
+capital
+cap_long
+cap_lat
+
+*/
+
+/* languages table fields are 
+
+lang_id
+code
+name
+percent
+official
+
+*/
+
+/* economies table fields are 
+
+econ_id
+code
+year 
+income_group
+gdp_percapita
+gross_savings
+inflation_rate
+total_investment
+unemployment_rate
+imports
+exports
+
+*/
+
+/* currencies table fields are 
+
+curr_id
+code
+basic_unit
+curr_code
+frac_unit
+frac_perbasic
+
+*/
+
+/* cities table fields are 
+
+name
+country_code
+city_proper_pop
+metroarea_pop
+urbanarea_pop
+
+*/
