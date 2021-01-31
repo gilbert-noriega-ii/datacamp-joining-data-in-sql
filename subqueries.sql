99f90fbbc1424ebf80c46c4f8fad30cb5ef3ad0b
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-01-31 16:19:59 -0600

    adding where clauses in subqueries

--- subqueries.sql
+++ subqueries.sql
@@ -89,3 +89,47 @@ income_group
 gross_savings
 
 */
+
+/*Begin by calculating the average life expectancy across all countries for 2015.*/
+
+SELECT AVG(life_expectancy)
+FROM populations
+WHERE year = 2015;
+
+/*Figure out which countries had high average life expectancies (at the country level) in 2015.*/
+
+SELECT * 
+FROM populations
+WHERE life_expectancy > 1.15 *
+   (SELECT AVG(life_expectancy)
+    FROM populations
+    WHERE year = 2015)
+AND year = 2015;
+
+/*Use your knowledge of subqueries in WHERE to get the urban area population for only capital cities.*/
+
+SELECT name, country_code, urbanarea_pop
+FROM cities
+WHERE name IN
+  (SELECT capital 
+   FROM countries)
+ORDER BY urbanarea_pop DESC;
+
+/*The code given in query.sql selects the top nine countries in terms of number of cities appearing in the cities table. Recall that this corresponds to the most populous cities in the world. Your task will be to convert the commented out code to get the same result as the code shown.*/
+
+/*SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;*/
+
+SELECT name AS country,
+  (SELECT COUNT(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
